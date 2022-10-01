70e6d02350c7653ad20132b5f25116d51b5f3688
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2022-10-02 00:10:17 +0530

    update

--- website/src/js/data.js
+++ website/src/js/data.js
@@ -29,13 +29,12 @@ let loader1 = new THREE.GLTFLoader();
 
 function init() {
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xfe6607);
-    scene.background = loaderB.load('../texture/orsky.jpg');
+    scene.background = new THREE.Color(0x110022);
+    //scene.background = loaderB.load('../texture/sky.webp');
     camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.01, 20000);
-    camera.position.x =11;
-    camera.position.y =0;
-    camera.position.y =15;
-    camera.rotation.z = 90;
+    camera.position.x = 5;
+    camera.position.y = 7;
+    camera.position.z = 15;
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     composer = new EffectComposer(renderer, renderTarget);
@@ -43,11 +42,10 @@ function init() {
     composer.addPass(new RenderPass(scene, camera));
     document.body.appendChild(renderer.domElement);
     //Bloom();
-    importSat();
-    composer.render();
     var yellowGlow = new THREE.AmbientLight(0xF4E99B, 1)
     scene.add(yellowGlow);
-
+    composer.render();
+    importSat();
 }
 
 
@@ -55,23 +53,25 @@ function init() {
 function importSat() {
     loader1.load('../models/observatory/scene.gltf', function (gltf) {
         sat = gltf.scene.children[0];
-        sat.scale.set(0.5,0.5, 0.5);
-        sat.position.set(0,0,0);
+        sat.scale.set(0.5, 0.5, 0.5);
+        sat.position.set(0, 0, 0);
         scene.add(gltf.scene);
         composer.render();
-        const geometry = new THREE.BoxGeometry( 1000, 0.1, 1000 );
-const material = new THREE.MeshBasicMaterial( {color: 0x111111} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-        controls = new OrbitControls(camera, renderer.domElement);
+        const geometry = new THREE.BoxGeometry(1000, 0.1, 1000);
+        const material = new THREE.MeshBasicMaterial({ color: 0x111111 });
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        //controls = new OrbitControls(camera, renderer.domElement);
         animateEarth();
     });
 }
 
 
 function animateEarth() {
-    console.log(camera.position.x, camera.position.y, camera.position.z);
-    controls.update();
+    //controls.update();
+    camera.position.z += 0.001;
+    camera.position.y += 0.001;
+    camera.position.x += 0.001;
     composer.render();
     requestAnimationFrame(animateEarth);
 }
